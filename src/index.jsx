d04e289e6fb0dc8e09167463df787f2434925df5
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-08-07 18:27:31 +0600

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { App } from "./App";
+import { createRoot } from "react-dom/client";
+
+// const root = document.getElementById("root");
+const root = createRoot(document.body);
+
+root.render(<App />);
